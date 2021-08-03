d4d333951bd7be7167062cec8a303f32a263e9c8
BeobSik <tjswl756@naver.com>
2021-08-03 15:12:51 +0900

    feat: test external configuration

--- src/main/java/com/example/springbootapplicationevent/SampleRunner.java
+++ src/main/java/com/example/springbootapplicationevent/SampleRunner.java
@@ -11,10 +11,14 @@ public class SampleRunner implements ApplicationRunner {
     @Value("${my.name}")
     private String name;
 
+    @Value("${my.age}")
+    private int age;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("====================");
         System.out.println(name);
+        System.out.println(age);
         System.out.println("====================");
     }
 }
--- src/test/java/com/example/springbootapplicationevent/SpringBootApplicationEventApplicationTests.java
+++ src/test/java/com/example/springbootapplicationevent/SpringBootApplicationEventApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.springbootapplicationevent;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootApplicationEventApplicationTests {
 
+    @Autowired
+    Environment environment;
+
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("my.name"))
+                .isEqualTo("BeobSik");
     }
 
 }
