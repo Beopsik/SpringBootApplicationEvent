90241da1aafca1a197f3480439eb5d6dd7fff52e
BeobSik <tjswl756@naver.com>
2021-08-04 13:41:16 +0900

    feat: type safe properties

--- src/main/java/com/example/springbootapplicationevent/SampleRunner.java
+++ src/main/java/com/example/springbootapplicationevent/SampleRunner.java
@@ -1,5 +1,6 @@
 package com.example.springbootapplicationevent;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,17 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${my.name}")
-    private String name;
-
-    @Value("${my.age}")
-    private int age;
+    @Autowired
+    BeobSikProperties beobSikProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("====================");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(beobSikProperties.getName());
+        System.out.println(beobSikProperties.getAge());
+        System.out.println(beobSikProperties.getFullName());
         System.out.println("====================");
     }
 }
